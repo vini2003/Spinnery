a035aa3a70529a2e0da0f905f0882469e47cc659
EngiN33R <painttool@gmail.com>
2020-01-27 20:39:01 +0300

    Add scrollbar to WList

--- src/main/java/spinnery/widget/WList.java
+++ src/main/java/spinnery/widget/WList.java
@@ -13,13 +13,15 @@ import java.util.List;
 import java.util.Map;
 
 @Environment(EnvType.CLIENT)
-public class WList extends WWidget implements WClient, WModifiableCollection, WFocusedMouseListener {
+public class WList extends WWidget implements WClient, WModifiableCollection {
 	public static final int SHADOW = 0;
 	public static final int BACKGROUND = 1;
 	public static final int HIGHLIGHT = 2;
 	public static final int OUTLINE = 3;
 	public static final int LABEL = 4;
 	public List<List<WWidget>> listWidgets = new ArrayList<>();
+
+	protected WScrollbar scrollbar;
 
 	public WList(WPosition position, WSize size, WInterface linkedInterface) {
 		setInterface(linkedInterface);
@@ -29,6 +31,11 @@ public class WList extends WWidget implements WClient, WModifiableCollection, WF
 		setSize(size);
 
 		setTheme("light");
+
+		scrollbar = new WScrollbar(linkedInterface, this);
+		scrollbar.setHidden(true);
+		linkedInterface.add(scrollbar);
+		updateScrollbar();
 	}
 
 	public static WWidget.Theme of(Map<String, String> rawTheme) {
@@ -41,30 +48,58 @@ public class WList extends WWidget implements WClient, WModifiableCollection, WF
 		return theme;
 	}
 
+	public void updateScrollbar() {
+		int scrollBarOffset = 2 + (hasLabel() ? 16 : 0);
+		int scrollBarWidth = 6;
+		int scrollBarHeight = size.getY() - 3 - scrollBarOffset;
+		int scrollBarX = position.getX() + size.getX() - scrollBarWidth - 3;
+		int scrollBarY = position.getY() + scrollBarOffset;
+		scrollbar.setPosition(WPosition.of(WType.FREE, scrollBarX, scrollBarY, getZ()));
+		scrollbar.setSize(WSize.of(scrollBarWidth, scrollBarHeight));
+	}
+
 	@Override
 	public void onMouseScrolled(int mouseX, int mouseY, double deltaY) {
+		if (isWithinBounds(mouseX, mouseY)) {
+			scroll(deltaY * 2.5);
+			super.onMouseScrolled(mouseX, mouseY, deltaY);
+		}
+	}
+
+	public void scroll(double deltaY) {
 		if (getListWidgets().size() == 0) {
 			return;
 		}
 
 		double scaledOffsetY = deltaY * 2.5;
+		int top = getY() + 4 + (hasLabel() ? 17 : 0);
+		int bottom = getY() + getHeight() - 9;
 
 		boolean hitTop = getListWidgets().get(0).stream().anyMatch(widget ->
-				widget.getY() + scaledOffsetY > getY() + 4
+				widget.getY() + scaledOffsetY > top
 		);
 
 		boolean hitBottom = getListWidgets().get(getListWidgets().size() - 1).stream().anyMatch(widget ->
-				widget.getY() + widget.getHeight() + scaledOffsetY <= getY() + getHeight() - 6
+				widget.getY() + widget.getHeight() + scaledOffsetY <= bottom
 		);
 
-		if ((scaledOffsetY > 0 && !hitTop) || (scaledOffsetY < 0 && !hitBottom)) {
-			for (WWidget widget : getWidgets()) {
+		///
+		int topScroll = getAnchorAtTop();
+		int topmostY = getListWidgets().get(0).stream().mapToInt(WWidget::getY).min().orElse(topScroll);
+		int bottomScroll = getAnchorAtBottom();
+		for (WWidget widget : getWidgets()) {
+			int yOffset = widget.getY() - topmostY;
+			if (scaledOffsetY > 0 && hitTop) {
+				widget.setY(topScroll + yOffset);
+				widget.setHidden(!(isWithinBounds(widget.getX(), widget.getY()) || isWithinBounds(widget.getX() + widget.getWidth(), widget.getY() + widget.getHeight())));
+			} else if (scaledOffsetY < 0 && hitBottom) {
+				widget.setY(bottomScroll + yOffset);
+				widget.setHidden(!(isWithinBounds(widget.getX(), widget.getY()) || isWithinBounds(widget.getX() + widget.getWidth(), widget.getY() + widget.getHeight())));
+			} else {
 				widget.setY(widget.getY() + (int) scaledOffsetY);
 				widget.setHidden(!(isWithinBounds(widget.getX(), widget.getY()) || isWithinBounds(widget.getX() + widget.getWidth(), widget.getY() + widget.getHeight())));
 			}
 		}
-
-		super.onMouseScrolled(mouseX, mouseY, deltaY);
 	}
 
 	public List<List<WWidget>> getListWidgets() {
@@ -97,6 +132,56 @@ public class WList extends WWidget implements WClient, WModifiableCollection, WF
 		super.setLabel(label);
 		updatePositions();
 		updateHidden();
+		updateScrollbar();
+	}
+
+	public boolean hasScroller() {
+		return !scrollbar.isHidden();
+	}
+
+	public void setScroller(boolean hasScroller) {
+		scrollbar.setHidden(!hasScroller);
+	}
+
+	public int getAnchorAtTop() {
+		int startY = getY() + 4;
+		if (hasLabel()) startY += 17;
+		return startY;
+	}
+
+	public int getAnchorAtBottom() {
+		if (getHeight() >= getInnerHeight()) return getAnchorAtTop();
+		int topY = getAnchorAtTop() - (getInnerHeight() - getHeight()) - 4 - 5;
+		if (hasLabel()) topY -= 17;
+		return topY;
+	}
+
+	public int getInnerHeight() {
+		int startY = getAnchorAtTop();
+		List<List<WWidget>> widgetLists = getListWidgets();
+		for (WWidget widget : widgetLists.get(0)) {
+			if (widget.getY() < startY) {
+				startY = widget.getY();
+			}
+		}
+		int endY = startY;
+		for (WWidget widget : widgetLists.get(widgetLists.size() - 1)) {
+			if (widget.getY() + widget.getHeight() > endY) {
+				endY = widget.getY() + widget.getHeight();
+			}
+		}
+		return endY - startY + 3; // 3px bottom padding to not hug the border
+	}
+
+	public int getTopOffset() {
+		int topY = getY() + (hasLabel() ? 16 : 0);
+		int lowestY = getY() + (hasLabel() ? 16 : 0);
+		for (WWidget widget : getListWidgets().get(0)) {
+			if (widget.getY() < lowestY) {
+				lowestY = widget.getY();
+			}
+		}
+		return topY - lowestY;
 	}
 
 	@Override
@@ -167,6 +252,8 @@ public class WList extends WWidget implements WClient, WModifiableCollection, WF
 		}
 
 		GL11.glDisable(GL11.GL_SCISSOR_TEST);
+
+		scrollbar.draw();
 	}
 
 	@Override
@@ -220,5 +307,76 @@ public class WList extends WWidget implements WClient, WModifiableCollection, WF
 			}
 		}
 		return false;
+	}
+
+	public static class WScrollbar extends WWidget implements WClient {
+		protected WList parent;
+		protected double clickMouseY;
+		protected boolean dragging = false;
+
+		public WScrollbar(WInterface linkedInterface, WList parent) {
+			setInterface(linkedInterface);
+			this.parent = parent;
+		}
+
+		public int getScrollerHeight() {
+			double outerHeight = getHeight();
+			double innerHeight = parent.getInnerHeight();
+			return (int) (outerHeight * (outerHeight / innerHeight));
+		}
+
+		public int getScrollerY() {
+			double outerHeight = getHeight();
+			double innerHeight = parent.getInnerHeight();
+			double topOffset = parent.getTopOffset();
+			double percentToEnd = topOffset / (innerHeight - outerHeight);
+			double maximumOffset = getHeight() - getScrollerHeight();
+			return getY() + (int) (maximumOffset * percentToEnd);
+		}
+
+		public void drawScroller() {
+			BaseRenderer.drawRectangle(getX(), getScrollerY(), getZ(), getWidth(), getScrollerHeight(), WColor.of("0x808080"));
+			BaseRenderer.drawRectangle(getX(), getScrollerY(), getZ(), getWidth() - 1, getScrollerHeight() - 1, WColor.of("0xC0C0C0"));
+		}
+
+		@Override
+		public void onMouseClicked(int mouseX, int mouseY, int mouseButton) {
+			if (mouseButton == 0) {
+				if (isWithinBounds(mouseX, mouseY)) {
+					if (mouseY >= getScrollerY() && mouseY <= getScrollerY() + getScrollerHeight()) {
+						dragging = true;
+						clickMouseY = mouseY - getScrollerY();
+					} else {
+						dragging = false;
+						if (mouseY > getScrollerY()) {
+							parent.scroll(-20);
+						} else {
+							parent.scroll(20);
+						}
+					}
+				} else {
+					dragging = false;
+				}
+			}
+			super.onMouseClicked(mouseX, mouseY, mouseButton);
+		}
+
+		@Override
+		public void onMouseDragged(int mouseX, int mouseY, int mouseButton, double deltaX, double deltaY) {
+			if (mouseButton == 0) {
+				if (dragging) {
+					double scrollerOffsetY = getScrollerY() + clickMouseY - mouseY;
+					parent.scroll(scrollerOffsetY / 2.5);
+				}
+			}
+			super.onMouseDragged(mouseX, mouseY, mouseButton, deltaX, deltaY);
+		}
+
+		@Override
+		public void draw() {
+			if (isHidden()) return;
+			BaseRenderer.drawRectangle(getX(), getY(), getZ(), getWidth(), getHeight(), WColor.of("0x000000"));
+			drawScroller();
+		}
 	}
 }
