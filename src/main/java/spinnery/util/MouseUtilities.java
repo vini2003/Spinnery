f121862a3b2177e5a7bc610d5d8c0a31fc70ccf0
vini2003 <vini.sch@outlook.com>
2020-04-07 19:51:59 -0300

    Improve MouseUtilities documentation.

--- src/main/java/spinnery/util/MouseUtilities.java
+++ src/main/java/spinnery/util/MouseUtilities.java
@@ -5,22 +5,33 @@ public class MouseUtilities {
 	public static int mouseX = 0, mouseY = 0;
 	public static long lastNanos = 0;
 
-	public static void lastNanos(long lastNanos) {
-		MouseUtilities.lastNanos = lastNanos;
-	}
-
+	/**
+	 * Retrieve the interval in nanoseconds since the last update.
+	 * @return Interval in nanoseconds since the last update.
+	 */
 	public static long nanoInterval() {
 		return System.nanoTime() - lastNanos();
 	}
 
+	/**
+	 * Retrieve the last time, in nanoseconds, when update was called.
+	 * @return The last time, in nanoseconds, when update was called.
+	 */
 	public static long lastNanos() {
 		return lastNanos;
 	}
 
+	/**
+	 * Retrieve the delay, in nanoseconds, used for calculations.
+	 * @return The delay, in nanoseconds.
+	 */
 	public static long nanoDelay() {
 		return nanoDelay;
 	}
 
+	/**
+	 * Update the last time, in nanoseconds, when update was called.
+	 */
 	public static void nanoUpdate() {
 		lastNanos = System.nanoTime();
 	}
