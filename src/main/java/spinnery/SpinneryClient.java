14fe39620766a6542ede3e35df9cc5134d4173af
vini2003 <vini.sch@outlook.com>
2020-05-02 21:12:52 -0300

    Remove debug stuff.

--- src/main/java/spinnery/SpinneryClient.java
+++ src/main/java/spinnery/SpinneryClient.java
@@ -1,24 +1,15 @@
 package spinnery;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.util.Identifier;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import spinnery.debug.ScreenRegistry;
 import spinnery.registry.NetworkRegistry;
 import spinnery.registry.ResourceRegistry;
 import spinnery.registry.WidgetRegistry;
 
 public class SpinneryClient implements ClientModInitializer {
-	public static final String LOG_ID = "Spinnery";
-	public static final Identifier MOD_ID = new Identifier(LOG_ID.toLowerCase());
-	public static Logger LOGGER = LogManager.getLogger("Spinnery");
-
 	@Override
 	public void onInitializeClient() {
 		NetworkRegistry.initializeClient();
 		WidgetRegistry.initialize();
 		ResourceRegistry.initialize();
-		ScreenRegistry.initialize();
 	}
 }
