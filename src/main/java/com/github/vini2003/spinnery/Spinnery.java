5510aeca5719a64bc53f56cb8a7eb8cbf7572cef
vini2003 <vini.sch@outlook.com>
2020-05-03 19:42:10 -0300

    An crime; an atrocity; an unforgivable act has been finished.

--- src/main/java/com/github/vini2003/spinnery/Spinnery.java
+++ src/main/java/com/github/vini2003/spinnery/Spinnery.java
@@ -1,18 +1,12 @@
 package com.github.vini2003.spinnery;
 
-import com.github.vini2003.spinnery.debug.*;
 import com.github.vini2003.spinnery.registry.ResourceRegistry;
-import com.github.vini2003.spinnery.registry.ThemeRegistry;
 import com.github.vini2003.spinnery.registry.WidgetRegistry;
-import net.minecraft.block.Block;
-import net.minecraft.inventory.container.ContainerType;
-import net.minecraft.resources.IReloadableResourceManager;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraft.resources.SimpleReloadableResourceManager;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,35 +21,18 @@ public class Spinnery {
 	public Spinnery() {
 		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-		eventBus.addGenericListener(Block.class, this::registerBlocks);
-		eventBus.addGenericListener(ContainerType.class, this::registerContainers);
 		eventBus.addListener(this::registerResources);
 		eventBus.addListener(this::initializeCommon);
-		eventBus.addListener(this::registerScreens);
 		eventBus.addListener(this::registerWidgets);
 	}
 
 	public void initializeCommon(FMLCommonSetupEvent event) {
 		NetworkRegistry.initialize();
-		BlockRegistry.initialize();
-		ContainerRegistry.initialize();
-	}
-
-	public void registerBlocks(RegistryEvent.Register<Block> blockRegister) {
-		BlockRegistry.register(blockRegister);
-	}
-
-	public void registerContainers(RegistryEvent.Register<ContainerType<?>> containerRegister) {
-		ContainerRegistry.register(containerRegister);
 	}
 
 	public void registerResources(FMLClientSetupEvent resourceRegister) {
 		ResourceRegistry.register(resourceRegister);
-		ResourceRegistry.register((FMLClientSetupEvent) resourceRegister.getMinecraftSupplier().get().getResourceManager());
-	}
-
-	public void registerScreens(FMLClientSetupEvent screenRegister) {
-		ScreenRegistry.register(screenRegister);
+		ResourceRegistry.load((SimpleReloadableResourceManager) resourceRegister.getMinecraftSupplier().get().getResourceManager());
 	}
 
 	public void registerWidgets(FMLClientSetupEvent widgetRegister) {
