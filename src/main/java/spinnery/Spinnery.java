13d9e15f1c4ceb4d3451a6941df960afbde28b49
vini2003 <vini.sch@outlook.com>
2020-07-08 15:08:31 -0300

    brrr

--- src/main/java/spinnery/Spinnery.java
+++ src/main/java/spinnery/Spinnery.java
@@ -5,22 +5,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import spinnery.common.configuration.registry.ConfigurationRegistry;
 import spinnery.common.registry.NetworkRegistry;
-import spinnery.debug.BlockRegistry;
-import spinnery.debug.ContainerRegistry;
-import spinnery.debug.ItemRegistry;
-import spinnery.debug.ScreenRegistry;
 
 public class Spinnery implements ModInitializer {
 	public static final String LOG_ID = "Spinnery";
 	public static final String MOD_ID = "spinnery";
-	public static Logger LOGGER = LogManager.getLogger("Spinnery");
+	public static Logger LOGGER = LogManager.getLogger(LOG_ID);
 
 	@Override
 	public void onInitialize() {
-		ItemRegistry.initialize();
-		BlockRegistry.initialize();
-		ContainerRegistry.initialize();
-		ScreenRegistry.initialize();
 		NetworkRegistry.initialize();
 		ConfigurationRegistry.initialize();
 	}
